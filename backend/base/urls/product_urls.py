801ffd90335712b069a194f561efd3f2867dbdf9
cj herenow <cj@cjs-Mac-mini.local>
2023-11-07 11:11:38 -0800

    first commit

---
+++ backend/base/urls/product_urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from base.views import product_views
+
+
+
+urlpatterns = [
+    path('', product_views.getProducts, name='products'),
+    path('upload/', product_views.uploadImage, name='image-upload'),
+    path('create/', product_views.createProduct, name='product-create'),
+    path('<str:pk>/reviews/', product_views.createProductReview, name='create-review'),
+    path('<str:pk>/', product_views.getProduct, name='product'),
+    path('delete/<str:pk>/', product_views.deleteProduct, name='product-delete',),
+    path('update/<str:pk>/', product_views.updateProduct, name='product-update'),
+]
